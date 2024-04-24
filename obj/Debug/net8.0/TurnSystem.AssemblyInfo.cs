3ce549d997e17ba57f0b3aa72ab945bdc1e0a203
DeklanBeckett <juandausuga16@gmail.com>
2024-04-24 18:50:53 -0500

    se crean los archivos necesarios para separar las vistas

--- obj/Debug/net8.0/TurnSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/TurnSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurnSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfdb756cd295028bee48881b900d0ecea0d07229")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425f48e92f0e9d35ce8eb2af3f8f0e01b7e49ced")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurnSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurnSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
