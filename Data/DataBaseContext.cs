e744c33cd62188077fb5388084d71ef9d2ae904a
DeklanBeckett <juandausuga16@gmail.com>
2024-04-23 21:33:12 -0500

    se creó el modelo adviser y shift

--- Data/DataBaseContext.cs
+++ Data/DataBaseContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TurnSystem.Models;
 
 
 namespace TurnSystem.Data{
@@ -7,5 +8,11 @@ namespace TurnSystem.Data{
         public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options){}
 
         //aqui se deben registrar los modelos
+
+        public DbSet<Adviser> Advisers  {get; set;}
+        public DbSet<Shift> Shifts  {get; set;}
+        
+
+
     }
 }
--- obj/Debug/net8.0/TurnSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/TurnSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurnSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e20134aa058d0cdc130b467b3708fd5f9d26def")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d5c1f5566747ac7b873af5ae03361b954350d3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurnSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurnSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
