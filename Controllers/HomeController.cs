83350e3a747eadb554ddc7d5ef85b43731bd68fe
Johan Arboleda <jhse757@gmail.com>
2024-04-26 16:58:51 -0500

    Se traen los datos de Main

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,30 +16,32 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-// Muestra vista Index
-
-    // public async Task<IActionResult> Index()
-    // {
-    //     return View(await _logger.Type_Users.ToListAsync());
-    // }
-
-// Muestra vista Shift
-   public IActionResult Shift()
-    {
-        return View();
-    }
-
-
    public IActionResult Index()
     {
         return View();
     }
 
-    // Muestra vista Type_users
-       public async Task<IActionResult> Type_Users()
-       
+    // Muestra la vista para seleccionar el tipo de usuario
+    [HttpGet]
+    public async Task<IActionResult> SelectTypeUser()
     {
-       return View(await _logger.Type_Users.ToListAsync());
+        var typeUsers = await _logger.Type_Users.ToListAsync();
+        return View(typeUsers);
+    }
+
+    // Redirección al método Shift después de seleccionar un tipo de usuario
+
+    // Muestra la vista de turnos utilizando el tipo de usuario seleccionado
+    [HttpGet]
+    public IActionResult Shift()
+    {
+        int typeUserId = 0;
+        if (TempData["Type_Users"] != null)
+        {
+            typeUserId = Convert.ToInt32(TempData["Type_Users"]);
+        }
+        ViewBag.Type_Users = typeUserId;
+        return View();
     }
 
 // Muestra vista Type_Procedure
