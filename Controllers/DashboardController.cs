10ba2c8ce1c846927ec2d87ea16477bcf7813506
DidierRmz <dder47@gmail.com>
2024-04-25 19:19:15 -0500

    Tercer commit

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -14,14 +14,28 @@ public class DashboardController : Controller
     }
 
     public async Task<IActionResult> Index()
-    {
-        var shifts = await _context.Shifts
-            .Include(s => s.User)
-            .Include(s => s.Adviser)
-            .Include(s => s.Type_Procedure)
-            .Include(s => s.Status)
-            .ToListAsync();
+{
+    // Obtener todos los turnos
+    var shifts = await _context.Shifts
+        .Include(s => s.User)
+        .Include(s => s.Adviser)
+        .Include(s => s.Type_Procedure)
+        .Include(s => s.Status)
+        .ToListAsync();
 
-        return View(shifts);
-    }
+    // Obtener el primer turno (por ejemplo, el más próximo en términos de fecha)
+    var firstShift = await _context.Shifts
+        .Include(s => s.User)
+        .Include(s => s.Adviser)
+        .Include(s => s.Type_Procedure)
+        .Include(s => s.Status)
+        .OrderBy(s => s.shift_date) // Ordenar por fecha del turno
+        .FirstOrDefaultAsync(); // Obtener el primer turno
+
+    // Pasar ambos resultados a la vista
+    ViewData["FirstShift"] = firstShift;
+
+    return View(shifts);
+}
+
 }
